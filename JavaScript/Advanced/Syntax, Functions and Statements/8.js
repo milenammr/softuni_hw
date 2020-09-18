1358c556f45ac930617386e62cb9aede0feca1ff
Milena <milena@Milenas-MacBook-Pro.local>
2020-09-18 16:58:08 +0300

    commit

--- JavaScript/Advanced/Syntax, Functions and Statements/8.js
+++ JavaScript/Advanced/Syntax, Functions and Statements/8.js
@@ -1,4 +1,14 @@
-function solve(){
-
+function solve(arr){
+    let sum = 0;
+    let sum2 = 0;
+    let concat = '';
+    for (let i of arr){
+        sum += i;
+        sum2 += 1 / i;
+        concat += i;
+    }
+    console.log(sum);
+    console.log(sum2);
+    console.log(concat);
 }
 solve([1, 2, 3])
